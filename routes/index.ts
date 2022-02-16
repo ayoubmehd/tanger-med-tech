ac6d5af67ad3c0515b47a4d57d4e326035e1e859
ayoubmehd <safi-_-gio-raja@live.fr>
2022-02-16 14:55:41 +0100

    feat: add auth route

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import auth from "./auth";
 import containers from "./container";
 
 const router = Router();
 
 router.use("/containers", containers);
+router.use(auth);
 
 export default router;
